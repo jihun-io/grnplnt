9df91cfa7cc787fcbc07a0e6b56f68976f62a36e
김지훈 <kim@jihun.io>
2024-05-02 01:44:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -217,9 +217,9 @@ def admin():
 def qna():
     return render_template('qna.html', selected_social = 'menu_bold')
 
-# @app.route('/static/files/혹성의%20아이%20캡스톤%20발표본.pdf')
-# def download_pdf():
-#     return send_file('/static/files/혹성의%20아이%20캡스톤%20발표본.pdf', as_attachment=True, minetype='application/pdf', attachment_filename='혹성의 아이 캡스톤 발표본.pdf')
+@app.route('/static/files/혹성의%20아이%20캡스톤%20발표본.pdf')
+def download_pdf():
+    return send_file('/static/files/혹성의%20아이%20캡스톤%20발표본.pdf', as_attachment=True, minetype='application/pdf', attachment_filename='혹성의 아이 캡스톤 발표본.pdf')
 
 host_addr = "0.0.0.0"
 port_num = "4062"
