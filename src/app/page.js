69b67e365b8692ec1f604662a3f8796c37645c2b
김지훈 <kim@jihun.io>
2024-09-02 01:10:11 +0900

    메인 화면에 임시로 Lorem 작성

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <main>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+        corporis aliquid voluptatem reiciendis cumque molestias omnis unde
+        repudiandae eius blanditiis corrupti, consequatur optio nulla. Dolore,
+        quod ipsa. Ipsa, eligendi natus.
+      </p>
+    </main>
+  );
 }
