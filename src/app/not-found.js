6389757de3562ae0973ede47a4a462079671e7b3
김지훈 <kim@jihun.io>
2024-09-02 01:09:54 +0900

    404 페이지 작성

---
+++ src/app/not-found.js
@@ -0,0 +1,12 @@
+// App Router (src/app/not-found.js)
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main>
+      <h1>404 - 페이지를 찾을 수 없습니다</h1>
+      <p>죄송합니다. 요청하신 페이지를 찾을 수 없습니다.</p>
+      <Link href="/">홈으로 돌아가기</Link>
+    </main>
+  );
+}
