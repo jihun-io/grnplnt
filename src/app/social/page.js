3398f41a0bd17537365b309d33a744a240faf21f
김지훈 <kim@jihun.io>
2024-09-02 21:47:08 +0900

    소셜 페이지 임시 작성

---
+++ src/app/social/page.js
@@ -0,0 +1,48 @@
+import styles from "@/styles/privacy.module.css";
+import { generateMetadata } from "/utils/metadata";
+
+const title = "소셜 - 혹성의 아이";
+const description = "소셜 페이지입니다.";
+
+export const metadata = generateMetadata(title, description);
+
+export const runtime = "edge";
+
+export default function social() {
+  return (
+    <main className="test">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque libero
+      asperiores, incidunt minus magnam labore facilis velit saepe mollitia
+      quisquam quos at dicta cumque repudiandae accusamus illum rerum maxime
+      doloribus! Adipisci ipsa sequi error dolores inventore ipsam voluptatibus
+      facilis, modi, nostrum, ut alias quas quasi earum. Provident nemo dicta
+      temporibus saepe vel exercitationem neque itaque consequatur? Officiis
+      totam beatae voluptate! Eius corrupti assumenda libero blanditiis esse ad
+      neque recusandae architecto alias, quia enim, dolore reiciendis. Cum ut
+      qui debitis aperiam culpa quam nemo, vero quis! Dolor repudiandae quidem
+      debitis earum. Recusandae provident mollitia maxime nobis doloremque
+      cumque atque nisi aliquam facere, vitae nostrum quia at incidunt suscipit
+      quis! Saepe voluptatum suscipit illo dolorem molestias atque a eveniet cum
+      deserunt recusandae! Cumque dolorem illo odit numquam voluptas
+      voluptatibus provident nostrum rem recusandae, fuga minima dolore
+      inventore eius, soluta veniam minus! Nulla, suscipit nesciunt? Blanditiis
+      totam itaque et possimus nostrum commodi fugit! Rerum vitae consectetur
+      nesciunt nam obcaecati debitis cum amet inventore natus hic minus facere,
+      doloremque minima? Quia exercitationem ad totam rerum magnam! Dolorem
+      dolorum soluta earum voluptatem aperiam excepturi fugiat? Tempora nisi
+      nostrum minima accusantium reprehenderit quam accusamus eos nam nulla!
+      Facilis dolor explicabo autem non dolore eius nulla corporis mollitia.
+      Magni nesciunt esse ab, numquam sequi deserunt temporibus. Alias. Vitae
+      nostrum harum perspiciatis maiores veniam ratione temporibus dolores et
+      omnis aut nulla tenetur sint, corporis illum nesciunt ipsa alias nam esse
+      officiis quis voluptatem recusandae? Delectus dolorum nemo pariatur?
+      Cupiditate doloremque sapiente dolor consequuntur accusantium, ratione
+      aliquam repellendus hic ipsa recusandae in non? Repellendus, est dolor
+      perspiciatis, iste ad magnam voluptatem possimus nemo cupiditate odio
+      recusandae, ab nam expedita. Vitae, porro. Vitae dicta voluptas nobis
+      obcaecati nihil rem id mollitia, maiores molestiae veniam voluptatum
+      pariatur accusantium. Maiores facilis quia iusto sequi non corporis, dicta
+      asperiores at quas illum doloremque!
+    </main>
+  );
+}
