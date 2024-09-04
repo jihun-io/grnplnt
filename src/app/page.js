00888d05fe442ffee06ca51cab392649bcaf94eb
김지훈 <kim@jihun.io>
2024-09-05 04:52:10 +0900

    테스트 푸시

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="px-6 md:px-8 lg:px-10 xl:px-12">
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+        Lorem ipsum dolor sit amet consecteturadipisicing elit. Tempora,
         corporis aliquid voluptatem reiciendis cumque molestias omnis unde
         repudiandae eius blanditiis corrupti, consequatur optio nulla. Dolore,
         quod ipsa. Ipsa, eligendi natus.
