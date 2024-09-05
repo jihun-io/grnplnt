516ca19e37acfd9c4bfd1079be8af3363ac93c44
김지훈 <kim@jihun.io>
2024-09-05 14:05:13 +0900

    스타일 수정

--- src/app/layout.js
+++ src/app/layout.js
@@ -97,14 +97,18 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ko" className="md:text-[17px] lg:text-[18px] xl:text-[19px]">
+    <html
+      lang="ko"
+      className="md:text-[17px] lg:text-[18px] xl:text-[19px] text-sugar-cane-950"
+    >
       <head>
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0"
         />
+        <link rel="stylesheet" href="https://use.typekit.net/rjd5iff.css" />
       </head>
-      <body className="min-h-[100vh] grid grid-rows-[auto,1fr,auto]">
+      <body className="min-h-[100vh] grid grid-rows-[auto,1fr,auto] bg-sugar-cane-50 box-border">
         <Header title={metadata.title} />
         {children}
         <Footer />
