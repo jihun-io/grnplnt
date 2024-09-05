4ce65e6a6879e5a6a6c98d285000a624c87157b6
김지훈 <kim@jihun.io>
2024-09-05 16:21:00 +0900

    form 컴포넌트 적용

--- src/app/social/guestbook/page.js
+++ src/app/social/guestbook/page.js
@@ -2,6 +2,7 @@ import { generateMetadata } from "/utils/metadata";
 import Link from "next/link";
 import SocialNav from "@/components/social-nav";
 import Button from "@/components/button";
+import GuestbookForm from "@/components/guestbook-form";
 
 const title = "소셜 - 혹성의 아이";
 const description = "혹성의 아이에 관한 소식을 만나 보세요.";
@@ -11,9 +12,9 @@ export const metadata = generateMetadata(title, description);
 export const runtime = "edge";
 
 async function getPosts() {
-  const API_URL = process.env.API_URL;
+  const API_LIST = process.env.API_URL + "guestbook/json";
   const API_KEY = process.env.API_KEY;
-  const response = await fetch(API_URL, {
+  const response = await fetch(API_LIST, {
     headers: {
       "X-API-Key": API_KEY,
     },
@@ -28,59 +29,21 @@ async function getPosts() {
 }
 
 export default async function Guestbook() {
+  const API_URL = process.env.API_URL;
+  const API_KEY = process.env.API_KEY;
+
   const posts = await getPosts();
   return (
     <main className="px-6 md:px-8 lg:px-10 xl:px-12">
       <h2 className="text-4xl font-bold mb-4">소셜</h2>
       <SocialNav />
-
       <section className="w-full flex flex-col items-center gap-8">
-        <form
-          className="w-full md:px-16 flex flex-col items-center gap-4"
-          action="/social/guestbook/submit"
-          method="POST"
-        >
-          <fieldset className="w-full flex flex-row flex-wrap justify-center items-center gap-x-4 gap-y-2">
-            <legend className="sr-only">사용자 정보</legend>
-            <label className="sr-only" htmlFor="user">
-              이름
-            </label>
-            <input
-              className="basis-6 flex-grow h-12 px-4 rounded-md shadow-md"
-              type="text"
-              id="user"
-              name="user"
-              placeholder="이름"
-            />
-
-            <label className="sr-only" htmlFor="password">
-              비밀번호
-            </label>
-            <input
-              className="w-fit flex-grow h-12 px-4 rounded-md shadow-md"
-              type="password"
-              id="password"
-              name="password"
-              placeholder="비밀번호"
-            />
-          </fieldset>
-
-          <label className="sr-only" htmlFor="content">
-            내용
-          </label>
-          <textarea
-            class="w-full p-4 rounded-md shadow-md"
-            id="content"
-            name="content"
-            rows="7"
-          ></textarea>
-          <Button type="submit">작성</Button>
-        </form>
+        <GuestbookForm API_URL={API_URL} API_KEY={API_KEY} />
         <ul className="w-11/12 flex flex-col gap-6 items-center">
           {posts.map(([title, date, content, id]) => (
             <li
               key={id}
-              className="flex flex-col w-9/12 bg-white gap-8 px-6 py-8 rounded-lg shadow-md shadow-slate-100 bg-[#fff]"
+              className="flex flex-col w-9/12 bg-white gap-8 px-6 py-8 rounded-lg shadow-md shadow-slate-100 bg-[#fff] relative overflow-hidden"
               id={id}
             >
               <div className="flex flex-row items-center flex-wrap justify-between">
@@ -88,6 +51,18 @@ export default async function Guestbook() {
                 <p className=" m-0 font-semibold">
                   <time>{date}</time>
                 </p>
+              </div>
+              <div className="absolute bottom-2 right-2">
+                <ul className="flex flex-row gap-x-2 justify-between items-center text-xs">
+                  <li>
+                    <Button className="px-2">수정</Button>
+                  </li>
+                  <li>
+                    <Button className="px-2" variant="red">
+                      삭제
+                    </Button>
+                  </li>
+                </ul>
               </div>
               <p className="m-0  break-keep text-left">{content}</p>
             </li>
