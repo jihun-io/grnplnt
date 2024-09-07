4fe7bea434a58b8117c0a70c8b2c3fb015158b6e
김지훈 <kim@jihun.io>
2024-09-07 14:58:59 +0900

    런타임 수정

--- src/app/social/guestbook/modify/[id]/page.js
+++ src/app/social/guestbook/modify/[id]/page.js
@@ -10,21 +10,13 @@ const GuestbookFormModify = dynamic(
 export const runtime = "edge";
 
 export default function ModifyEntry({ params }) {
-  const API_URL = process.env.API_URL;
-  const API_KEY = process.env.API_KEY;
-
   const sn = params.id;
 
   return (
     <main className="px-6 md:px-8 lg:px-10 xl:px-12">
       <h2 className="text-4xl font-bold mb-4">소셜</h2>
       <section className="w-full flex flex-col items-center gap-8">
-        <GuestbookFormModify
-          API_URL={API_URL}
-          API_KEY={API_KEY}
-          id={sn}
-          type="modify"
-        />
+        <GuestbookFormModify id={sn} type="modify" />
       </section>
     </main>
   );
