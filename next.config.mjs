5b55fec5e428891e459266e49069cf1ee6f0f782
김지훈 <kim@jihun.io>
2024-09-08 12:12:45 +0900

    캐러셀 관련 Bootstrap 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "node_modules")],
+  },
+};
 
 if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
