1314c3039f4985ce24c7de5fca3dc44ff7ca7bb3
김지훈 <kim@jihun.io>
2024-09-09 08:58:19 +0900

    색상 수정

--- src/components/button.js
+++ src/components/button.js
@@ -15,9 +15,9 @@ const Button = ({
       "bg-sugar-cane-500 hover:bg-sugar-cane-600 active:bg-sugar-cane-700 hover:text-sugar-cane-50 active:text-sugar-cane-50",
     outline:
       "border-solid border-2 border-sugar-cane-700 bg-sugar-cane-50 text-sugar-cane-950 hover:bg-sugar-cane-200 active:bg-sugar-cane-300",
-    red: "bg-wisp-pink-500 hover:bg-wisp-pink-600 active:bg-wisp-pink-700 text-wisp-pink-50 ",
+    red: "bg-chablis-500 hover:bg-chablis-600 active:bg-chablis-700 text-chablis-50 ",
     red_outline:
-      "border-solid border-2 border-wisp-pink-700 bg-wisp-pink-50 text-wisp-pink-950 hover:bg-wisp-pink-200 active:bg-wisp-pink-300",
+      "border-solid border-2 border-chablis-700 bg-chablis-50 text-chablis-950 hover:bg-chablis-200 active:bg-wisp-pink-300",
     blue: "bg-blue-500 hover:bg-blue-600 active:bg-blue-700 text-white hover:text-blue-50 active:text-blue-50",
     // 필요에 따라 더 많은 변형을 추가할 수 있습니다.
   };
