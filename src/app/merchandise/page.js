555898d9415826842ec517143bf54c9fe4a7e304
김지훈 <kim@jihun.io>
2024-10-24 02:46:25 +0900

    패딩 추가

--- src/app/merchandise/page.js
+++ src/app/merchandise/page.js
@@ -8,7 +8,7 @@ export const metadata = generateMetadata(title, description);
 
 export default function privacy() {
   return (
-    <main className="flex flex-col justify-center items-center capitalize futura text-3xl">
+    <main className="flex flex-col justify-center items-center capitalize px-8 text-3xl">
       <p className="sr-only">Green Planet Merchandise Coming Soon</p>
       <Image width={320} height={320} src="/images/cs-text.svg" />
     </main>
