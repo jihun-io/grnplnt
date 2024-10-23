676a61f4e6e8f1456ac47a7a596c4ed720299907
김지훈 <kim@jihun.io>
2024-10-23 22:18:43 +0900

    이미지 리사이징 해상도 지정

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "node_modules")],
   },
+  images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
 };
 
 if (process.env.NODE_ENV === "development") {
