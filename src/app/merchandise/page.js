63ea42f81de4cc07f4b369bf574c2cef25048881
김지훈 <kim@jihun.io>
2024-10-23 22:18:25 +0900

    Futura 텍스트를 SVG로 대체

--- src/app/merchandise/page.js
+++ src/app/merchandise/page.js
@@ -1,4 +1,5 @@
 import { generateMetadata } from "/utils/metadata";
+import Image from "next/image";
 
 const title = "상품 - 혹성의 아이";
 const description = "혹성의 아이의 공식 상품을 만나 보세요.";
@@ -8,7 +9,8 @@ export const metadata = generateMetadata(title, description);
 export default function privacy() {
   return (
     <main className="flex flex-col justify-center items-center capitalize futura text-3xl">
-      <p>Green Planet Merchandise</p> <p>Coming Soon</p>
+      <p className="sr-only">Green Planet Merchandise Coming Soon</p>
+      <Image width={320} height={320} src="/images/cs-text.svg" />
     </main>
   );
 }
