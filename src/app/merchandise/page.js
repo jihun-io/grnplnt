ba659d614ad2a7ddb74d582cc53d4dc0fa1d4497
김지훈 <kim@jihun.io>
2024-10-24 09:44:55 +0900

    상품 페이지 업데이트
    상품 캐러셀 추가

--- src/app/merchandise/page.js
+++ src/app/merchandise/page.js
@@ -1,5 +1,6 @@
 import { generateMetadata } from "/utils/metadata";
 import Image from "next/image";
+import Merch from "@/components/merch";
 
 const title = "상품 - 혹성의 아이";
 const description = "혹성의 아이의 공식 상품을 만나 보세요.";
@@ -9,8 +10,15 @@ export const metadata = generateMetadata(title, description);
 export default function privacy() {
   return (
     <main className="flex flex-col justify-center items-center capitalize px-8 text-3xl">
+      <h2 className="sr-only">상품</h2>
       <p className="sr-only">Green Planet Merchandise Coming Soon</p>
-      <Image width={320} height={320} src="/images/cs-text.svg" />
+      <Merch />
+      <Image
+        width={320}
+        height={320}
+        className="mb-32"
+        src="/images/cs-text.svg"
+      />
     </main>
   );
 }
