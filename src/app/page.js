c72db4925f9a03efc89ca40253953b064fd7ce99
김지훈 <kim@jihun.io>
2024-11-07 23:12:36 +0900

    class를 className으로 수정

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
         <h3 className="sr-only">메인 이미지</h3>
         <HeroCarousel />
       </section>
-      <hr class="mx-32 my-16 border-sugar-cane-950"></hr>
+      <hr className="mx-32 my-16 border-sugar-cane-950"></hr>
       <section className="synopsis w-full h-[calc(100vh-4rem-2.5rem)] bg-[url('/optimized/synopsis-background_1920-1080.webp')] bg-cover flex flex-col justify-center items-center">
         <h3 className="pyeongchangpeace-bold text-sugar-cane-50 text-4xl md:text-5xl lg:text-6xl mb-16">
           SYNOPSIS
@@ -38,9 +38,10 @@ export default function Home() {
           allowFullScreen
           loading="lazy"
           className="w-full aspect-video"
+          title="혹성의 아이 예고편"
         ></iframe>
       </section>
-      <hr class="mx-32 my-32 border-sugar-cane-950"></hr>
+      <hr className="mx-32 my-32 border-sugar-cane-950"></hr>
       <section className="character w-full h-fit">
         <h3 className="sr-only">캐릭터</h3>
         <ul>
