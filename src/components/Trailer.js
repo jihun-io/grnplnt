e8f347f9c6dd08185333b7946ab3fd7eb49d86ff
김지훈 <kim@jihun.io>
2024-11-08 09:19:38 +0900

    예고편 섬네일 이미지 최적화

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -2,6 +2,7 @@
 
 import YouTube from "react-youtube";
 import React, { useState } from "react";
+import OptimizedImage from "./OptimizedImage";
 
 export default function Trailer({ videoId, className }) {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -20,36 +21,30 @@ export default function Trailer({ videoId, className }) {
         role="button"
         aria-label="예고편 재생하기"
       >
-        <img
-          src={thumbnailUrl}
+        <OptimizedImage
+          src="/images/thumbnail.jpeg"
           alt="혹성의 아이 예고편"
           className="w-full h-full object-cover"
         />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="w-16 h-12 bg-red-600 rounded-lg flex items-center justify-center">
-            <div className="w-0 h-0 border-t-8 border-b-8 border-l-12 border-transparent border-l-white ml-1">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="#64b900"
-                viewBox="0 0 24 24"
-                strokeWidth={1}
-                stroke="currentColor"
-                className="size-24 -translate-x-1/2 -translate-y-1/2 opacity-90"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
-                />
-              </svg>
-            </div>
-          </div>
-        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#64b900"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="size-24 -translate-x-1/2 -translate-y-1/2 opacity-90 absolute top-1/2 left-1/2 inset-0 flex items-center justify-center"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z"
+          />
+        </svg>
       </div>
     );
   }
